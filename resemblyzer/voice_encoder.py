4c601041467de9af1ad24a2f7585b28b0a62d138
NoahDrisort <xojziyfay@gmail.com>
2020-11-15 22:31:27 +0700

    pretrain

--- resemblyzer/voice_encoder.py
+++ resemblyzer/voice_encoder.py
@@ -30,7 +30,7 @@ class VoiceEncoder(nn.Module):
         self.device = device
             
         # Load the pretrained model'speaker weights
-        weights_fpath = Path(__file__).resolve().parent.joinpath("pretrained.pt")
+        weights_fpath = Path(__file__).resolve().parent.joinpath("first_model.pt")
         if not weights_fpath.exists():
             raise Exception("Couldn't find the voice encoder pretrained model at %s." % 
                             weights_fpath)
